3b874c2d4356981a263d35559dafd920f7affdbe
anubhav <anubhav@gnani.ai>
2020-03-17 20:21:07 +0530

    Updates

---
+++ precache-manifest.ff4ae7f37043a0aca0b4ac716b450fc3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "098a7a5b172b21b8a2355c9af7bbe991",
+    "url": "/Weather/index.html"
+  },
+  {
+    "revision": "22743c204d1a46802ce8",
+    "url": "/Weather/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "aede4d6f91553e58ada1",
+    "url": "/Weather/static/css/main.a5a4a9e2.chunk.css"
+  },
+  {
+    "revision": "22743c204d1a46802ce8",
+    "url": "/Weather/static/js/2.2475b3f8.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Weather/static/js/2.2475b3f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aede4d6f91553e58ada1",
+    "url": "/Weather/static/js/main.4547abd3.chunk.js"
+  },
+  {
+    "revision": "6afc3a9aec90fb07cf1d",
+    "url": "/Weather/static/js/runtime-main.08978631.js"
+  },
+  {
+    "revision": "dfc0cda63aba4a842c70693be2325975",
+    "url": "/Weather/static/media/loading.dfc0cda6.gif"
+  },
+  {
+    "revision": "b626fbd5fa7acd4b6442fd73597379e4",
+    "url": "/Weather/static/media/weatherLogo.b626fbd5.svg"
+  }
+]);
